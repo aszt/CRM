d24fd7a6afd19700e3fcfb0286c20303729cb3d0
386592336@qq.com <386592336@qq.com>
2018-09-05 16:12:09 +0800

    整合orm

--- src/main/java/com/crm/dao/impl/CustomerDaoImpl.java
+++ src/main/java/com/crm/dao/impl/CustomerDaoImpl.java
@@ -2,16 +2,18 @@ package com.crm.dao.impl;
 
 import com.crm.dao.CustomerDao;
 import com.crm.domain.Customer;
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 
 /**
  * 客户管理DAO层的实现类
  * @author tong
  *
  */
-public class CustomerDaoImpl implements CustomerDao {
+public class CustomerDaoImpl extends HibernateDaoSupport implements CustomerDao {
 
     @Override
     public void save(Customer customer) {
         System.out.println("CustomerDao中的save方法调用了...");
+        this.getHibernateTemplate().save(customer);
     }
 }
--- src/main/java/com/crm/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/crm/service/impl/CustomerServiceImpl.java
@@ -3,11 +3,14 @@ package com.crm.service.impl;
 import com.crm.dao.CustomerDao;
 import com.crm.domain.Customer;
 import com.crm.service.CustomerService;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * 客户管理业务层实现类
  * @author tong
  *
  */
+@Transactional
 public class CustomerServiceImpl implements CustomerService {
 
     // 注入DAO
